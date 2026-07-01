3efeb8f377668ec65711580b1923654be9ca8ab6
CR-Aylin <147929737+CR-Aylin@users.noreply.github.com>
2026-07-01 02:59:15 -0400

    model

--- src/model/KNN.py
+++ src/model/KNN.py
@@ -1,1 +1,22 @@
-#KNN
+import numpy as np
+from collections import Counter
+
+class KNN:
+    def __init__(self, k=5):
+        self.k = k
+        self.X_train = None
+        self.y_train = None
+    
+    def fit(self, X, y):
+        self.X_train = np.array(X)
+        self.y_train = np.array(y)
+    
+    def predict(self, X):
+        X = np.array(X)
+        return [self._predict_single(x) for x in X]
+    
+    def _predict_single(self, x):
+        distances = np.sqrt(np.sum((self.X_train - x)**2, axis=1))
+        k_indices = np.argsort(distances)[:self.k]
+        k_labels = self.y_train[k_indices]
+        return Counter(k_labels).most_common(1)[0][0]
--- src/model/NaiveBayes.py
+++ src/model/NaiveBayes.py
@@ -1,1 +1,32 @@
-#KNN
+import numpy as np
+
+class NaiveBayes:
+    def __init__(self):
+        self.classes = None
+        self.mean = {}
+        self.var = {}
+        self.prior = {}
+    
+    def fit(self, X, y):
+        X = np.array(X)
+        self.classes = np.unique(y)
+        
+        for c in self.classes:
+            X_c = X[y == c]
+            self.mean[c] = np.mean(X_c, axis=0)
+            self.var[c] = np.var(X_c, axis=0) + 1e-9
+            self.prior[c] = len(X_c) / len(X)
+    
+    def predict(self, X):
+        X = np.array(X)
+        return [self._predict_single(x) for x in X]
+    
+    def _predict_single(self, x):
+        posteriors = {}
+        for c in self.classes:
+            # Probabilidad gaussiana
+            exponent = np.exp(-((x - self.mean[c])**2) / (2 * self.var[c]))
+            prob = (1 / np.sqrt(2 * np.pi * self.var[c])) * exponent
+            posteriors[c] = np.sum(np.log(prob)) + np.log(self.prior[c])
+        
+        return max(posteriors, key=posteriors.get)
