d6a3ba5cdf56f968e0c4360bca563f2f8fe1b583
Alumno <alumno@academic.local>
2026-07-09 17:16:46 -0400

    wonito

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import cv2
+import time
 import numpy as np
 from pydobot import Dobot
-import serial.tools.list_ports as list_ports  # Cambio importante
-import time
+import serial.tools.list_ports as list_ports 
+import time # time.sleep(3)  # Espera 3 segundos
 import os
 
 import src.robot.Calibracion as c
@@ -10,44 +11,63 @@ import src.robot.camara as cama
 import src.model.Extraccion_caracteristicas as ec
 import src.robot.dobot_movement as dm
 
-
-
-ROBOTS = dm.conectar()
-#//////////////////////////////////////////////////////////////
-dm.Home(ROBOTS)
-
-cam = cama.Cam()
-resultados = cam.sacar_foto("Prueba1")
-
-print(resultados)
-
-#Aqui Colocar Algoritmos una vez funcionen
-#una vez identificado el objeto , mover robot
-
-#pixel_x, pixel_y, = resultado["puntos"]
-
-pixel_x, pixel_y, = resultados["referencia"]
-pose = ROBOTS.pose()
-robot_x, robot_y = pose[0] , pose[1]
-
-coor , offsetx , offsety  = c.coordernadas_n(pixel_x, pixel_y, robot_x, robot_y) # una vez
-
-print(coor)
-pose = ROBOTS.pose()
-print(pose)
+#modelos
+import KNN as knn
 
 
 
-Npixel_x = 169
-Npixel_y = 381
+def Ejemplo(ROBOTS):
+    
+    Npixel_x = 169
+    Npixel_y = 381
 
+    X,Y = c.pixeles_a_mm(Npixel_x, Npixel_y, offsetx, offsety)
+    print(X,Y)
+    ROBOTS.move_to(X, Y,50,0, wait=True)
 
-X,Y = c.pixeles_a_mm(Npixel_x, Npixel_y, offsetx, offsety)
-print(X,Y)
+if __name__ == "__main__":
+        
+    ROBOTS = dm.conectar() #inicia
+    dm.Home(ROBOTS)
 
-ROBOTS.move_to(X, Y,50,0, wait=True)
+    cam = cama.Cam()
+    resultados = cam.sacar_foto("Prueba1")
 
-#dm.mover_robot(ROBOTS,Cordenaa1,Cordenaa2)
+    #print(resultados)
+    
+    pixel_x, pixel_y, = resultados["referencia"]
+    pose = ROBOTS.pose()
+    robot_x, robot_y = pose[0] , pose[1]
+
+    coor , offsetx , offsety  = c.coordernadas_n(pixel_x, pixel_y, robot_x, robot_y)
+    pose = ROBOTS.pose()
+
+    print(f"Coordenadas Calculadas = {coor}")
+    print(f"Coordenadas Actual = {pose} ")
+
+    #Aqui Colocar Algoritmos una vez funcionen
+    modelo = int(input("Seleccione Modelo: "))
+    redu = bool(int(input("Reducción Dimensional (1/0): "))) #boleano
+
+    match (modelo, redu):
+        case (1, TRUE):
+            print("KNN - PCA")
+            
+        case (1, False):
+            print("KNN - PCA")
+        case (2, TRUE):
+            print("NaivesBayes - PCA")
+        case (2, False):
+            print("NaivesBayes - LDA")
+        case (3, TRUE):
+            print("SVM - PCA")
+        case (3, False):
+            print("SVM - LDA")
+        case _:
+            print("Combinación no válida")
+
+#una vez identificado el objeto , mover robot , coordenaa1 == objetos a mover ; coordenada 2 == donde dejar el objeto
+#   #dm.mover_robot(ROBOTS,Cordenaa1,Cordenaa2)
 
 
 
